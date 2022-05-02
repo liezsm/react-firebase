7c09877562f4dbdcf02d05f3bda4331c3a1853b1
liezsm <liez.dev@gmail.com>
2022-05-03 07:42:21 +0800

    Fetching data from database to post

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+// firebase get data
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../firebase-config";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [data, setDate] = useState();
+
+  // params: (dbconfig, name of table)
+  const postsCollectionRef = collection(db, "posts");
+
+  // fetch the data from database firebase
+  useEffect(() => {
+    const getPost = async () => {
+      const data = await getDocs(postsCollectionRef);
+      console.log(data.docs.map((item) => ({ ...item.doc, id: item.id })));
+    };
+
+    getPost();
+  }, []);
+  return <div className='homePage'>homepage</div>;
 };
 
 export default Home;
