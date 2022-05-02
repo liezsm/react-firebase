5cc9624f41008ec3b67a01e71308ba00a1c8e6cb
liezsm <liez.dev@gmail.com>
2022-05-02 13:55:25 +0800

    Login/logout functionality using firebase added

--- src/App.js
+++ src/App.js
@@ -9,17 +9,40 @@ import CreatePost from "./pages/CreatePost";
 // routing
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
+import { useState } from "react";
+
+// firebase auth
+
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
+
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+
+  const onSignOut = () => {
+    // call the method from firebase
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      // redirect to another page after logging out
+
+      window.location.pathname = "/login";
+    });
+  };
   return (
     <Router>
       <nav>
         <Link to='/'>Home </Link>
         <Link to='/create-post'>Create </Link>
-        <Link to='/login'>Login </Link>
+        {!isAuth ? (
+          <Link to='/login'>Login </Link>
+        ) : (
+          <button onClick={onSignOut}> Log out</button>
+        )}
       </nav>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/login' element={<Login login={setIsAuth} />} />
         <Route path='/create-post' element={<CreatePost />} />
       </Routes>
     </Router>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,31 @@
 import React from "react";
 
-const Login = () => {
-  return <div>Login</div>;
+// use the google auth when butotn click
+
+import { auth, provider } from "../firebase-config";
+import { signInWithPopup } from "firebase/auth";
+
+// routing
+import { useNavigate } from "react-router-dom";
+
+const Login = ({ login }) => {
+  let navigate = useNavigate();
+  const signInWithGoggle = () => {
+    signInWithPopup(auth, provider).then((res) => {});
+    // to ddetermine if logged in
+    localStorage.setItem("isauth", true);
+    login(true);
+    // after logging in redirecct to home page
+    navigate("/");
+  };
+  return (
+    <div className='loginPage'>
+      <p>Sign in with Goggle</p>
+      <button className='login-with-google' onClick={signInWithGoggle}>
+        Sign in With Google
+      </button>
+    </div>
+  );
 };
 
 export default Login;
